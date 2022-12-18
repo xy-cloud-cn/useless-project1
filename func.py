2c4ff06b1121db2d8af82007eb455c0b365b78a4
xy_cloud <85053859+xy-cloud-cn@users.noreply.github.com>
2022-12-18 21:16:26 +0800

    useless

--- func.py
+++ func.py
@@ -491,9 +491,9 @@ def main(rev):
             send_msg({'msg_type': 'group', 'number': group,
                       'msg': f'Ban [CQ:at,qq={vote[1]}]还剩{60 - (time.time() - vote[0])}秒\n'
                              f'理由:{vote[5]}\n'
-                             f'{f3_icon * int(vote[2] / 2) + empty_icon * int(5 - vote[2] / 2)}|{empty_icon * int(5 - vote[3] / 2) + f4_icon * int(vote[3] / 2)}\n'
+                             f'{f3_icon * (vote[2] // 2) + empty_icon * (5 - vote[2] // 2)}|{empty_icon * (5 - vote[3] // 2) + f4_icon * (vote[3] // 2)}\n'
                              f'f3-赞成 f4-反对'})
-        elif rev['message'].upper=='F3':
+        elif rev['message'].upper()=='F3':
             if not vote:
                 return
             if time.time()-vote[0]>60:
@@ -513,9 +513,9 @@ def main(rev):
                 send_msg({'msg_type': 'group', 'number': group,
                           'msg': f'Ban [CQ:at,qq={vote[1]}]还剩{60 - (time.time() - vote[0])}秒\n'
                                  f'理由:{vote[5]}\n'
-                                 f'{f3_icon * int(vote[2] / 2) + empty_icon * int(5 - vote[2] / 2)}|{empty_icon * int(5 - vote[3] / 2) + f4_icon * int(vote[3] / 2)}\n'
+                                 f'{f3_icon * (vote[2] // 2) + empty_icon * (5 - vote[2] // 2)}|{empty_icon * (5 - vote[3] // 2) + f4_icon * (vote[3] // 2)}\n'
                                  f'f3-赞成 f4-反对'})
-        elif rev['message'].upper=='F4':
+        elif rev['message'].upper()=='F4':
             if not vote:
                 return
             if time.time()-vote[0]>60:
