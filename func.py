12af01b0f1fed624eaa2b6736b71a1e27fa11bb3
xy_cloud <85053859+xy-cloud-cn@users.noreply.github.com>
2022-12-18 21:14:10 +0800

    useless

--- func.py
+++ func.py
@@ -488,10 +488,11 @@ def main(rev):
             else:
                 vote.append('')
             send_msg({'msg_type': 'group', 'number': group, 'msg': f'\'{card}\' called for vote to mute \'[CQ:at,qq={vote[1]}]\' for 15 minutes({vote[5]})'})
-            send_msg({'msg_type': 'group', 'number': group, 'msg': f'Ban [CQ:at,qq={vote[1]}]还剩{60-(time.time()-vote[0])}秒\n'
-                                                                   f'理由:{vote[5]}\n'
-                                                                   f'{f3_icon*(vote[2]/2)+empty_icon*(5-vote[2]/2)}|{empty_icon*(5-vote[3]/2)+f4_icon*(vote[3]/2)}\n'
-                                                                   f'f3-赞成 f4-反对'})
+            send_msg({'msg_type': 'group', 'number': group,
+                      'msg': f'Ban [CQ:at,qq={vote[1]}]还剩{60 - (time.time() - vote[0])}秒\n'
+                             f'理由:{vote[5]}\n'
+                             f'{f3_icon * int(vote[2] / 2) + empty_icon * int(5 - vote[2] / 2)}|{empty_icon * int(5 - vote[3] / 2) + f4_icon * int(vote[3] / 2)}\n'
+                             f'f3-赞成 f4-反对'})
         elif rev['message'].upper=='F3':
             if not vote:
                 return
@@ -512,7 +513,7 @@ def main(rev):
                 send_msg({'msg_type': 'group', 'number': group,
                           'msg': f'Ban [CQ:at,qq={vote[1]}]还剩{60 - (time.time() - vote[0])}秒\n'
                                  f'理由:{vote[5]}\n'
-                                 f'{f3_icon * (vote[2] / 2) + empty_icon * (5 - vote[2] / 2)}|{empty_icon * (5 - vote[3] / 2) + f4_icon * (vote[3] / 2)}\n'
+                                 f'{f3_icon * int(vote[2] / 2) + empty_icon * int(5 - vote[2] / 2)}|{empty_icon * int(5 - vote[3] / 2) + f4_icon * int(vote[3] / 2)}\n'
                                  f'f3-赞成 f4-反对'})
         elif rev['message'].upper=='F4':
             if not vote:
